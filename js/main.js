b9c8843c294846ac24e8a0295bb45f202cfaf657
root <root@CCPOMPEY40.CC-BASSINPOMPEY.LOCAL>
2017-05-29 15:28:50 +0200

    20170529-1528

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /*jslint browser: true, devel: true, node: true, sloppy: true, regexp: true */
-/*global $, Vue, VueRouter*/
+/*global $, Vue, VueRouter, donnees*/
+
 
 var substringMatcher = function (strs) {
     return function findMatches(q, cb) {
@@ -22,16 +23,7 @@ var substringMatcher = function (strs) {
         cb(matches);
     };
 },
-    states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-          'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-          'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-          'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-          'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-          'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-          'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-          'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-          'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-             ],
+    
     compRecherche = {
         template: '#page-recherche-template',
         methods: {
@@ -43,50 +35,7 @@ var substringMatcher = function (strs) {
     compTags = {
         template: '#page-tags-template',
         data : function () { return {
-            tags: [
-                {text: "Lorem", weight: 13},
-                {text: "Ipsum", weight: 10.5},
-                {text: "Dolor", weight: 9.4},
-                {text: "Sit", weight: 8},
-                {text: "Amet", weight: 6.2},
-                {text: "Consectetur", weight: 5},
-                {text: "Adipiscing", weight: 5},
-                {text: "Elit", weight: 5},
-                {text: "Nam et", weight: 5},
-                {text: "Leo", weight: 4},
-                {text: "Sapien", weight: 4},
-                {text: "Pellentesque", weight: 3},
-                {text: "habitant", weight: 3},
-                {text: "morbi", weight: 3},
-                {text: "tristisque", weight: 3},
-                {text: "senectus", weight: 3},
-                {text: "et netus", weight: 3},
-                {text: "et malesuada", weight: 3},
-                {text: "fames", weight: 2},
-                {text: "ac turpis", weight: 2},
-                {text: "egestas", weight: 2},
-                {text: "Aenean", weight: 2},
-                {text: "vestibulum", weight: 2},
-                {text: "elit", weight: 2},
-                {text: "sit amet", weight: 2},
-                {text: "metus", weight: 2},
-                {text: "adipiscing", weight: 2},
-                {text: "ut ultrices", weight: 2},
-                {text: "justo", weight: 1},
-                {text: "dictum", weight: 1},
-                {text: "Ut et leo", weight: 1},
-                {text: "metus", weight: 1},
-                {text: "at molestie", weight: 1},
-                {text: "purus", weight: 1},
-                {text: "Curabitur", weight: 1},
-                {text: "diam", weight: 1},
-                {text: "dui", weight: 1},
-                {text: "ullamcorper", weight: 1},
-                {text: "id vuluptate ut", weight: 1},
-                {text: "mattis", weight: 1},
-                {text: "et nulla", weight: 1},
-                {text: "Sed", weight: 1}
-            ]
+            tags: donnees.tags
         }; }
     },
     compArborescence = {
@@ -134,13 +83,110 @@ Vue.component('in-recherche', {
             },
             {
                 name: 'states',
-                source: substringMatcher(states)
+                source: substringMatcher(donnees.states)
             }
         ).on('typeahead:select', function (event, suggession) {
             me.$emit('change', suggession);
         }).on('keyup', function (e) {
             if (e.originalEvent.keyCode === 13) {
                 me.$emit('change', $(this).val());
+            }
+        });
+    }
+});
+
+Vue.component('in-tags', {
+    template : '<select multiple></select>',
+    mounted : function () {
+        $(this.$el).tokenize2({tokensAllowCustom: true, dataSource: 'http://gopala.fr/donnees.php', searchFromStart : false});
+    }
+});
+
+Vue.component('in-fichier', {
+    template : '<form action="http://www.gopala.fr/upload.php" method="post" enctype="multipart/form-data"><input type="file" name="files"></form>',
+    mounted : function () {
+        var me = this;
+        
+        $(this.$el).find('input').fileuploader({
+            changeInput: '<div class="fileuploader-input">' +
+                              '<div class="fileuploader-input-inner">' +
+                                  '<img src="images/upload.png"><br>' +
+                                  'Déposer le fichier ici ou cliquer pour parcourir' +
+                              '</div>' +
+                          '</div>',
+            theme: 'dragdrop',
+            limit: 1,
+            upload: {
+                url: 'http://www.gopala.fr/upload.php',
+                data: null,
+                type: 'POST',
+                enctype: 'multipart/form-data',
+                start: true,
+                synchron: true,
+                beforeSend: null,
+                onSuccess: function (result, item) {
+                    var data = JSON.parse(result), warning;
+
+                    // if success
+                    if (data.isSuccess && data.files[0]) {
+                        item.name = data.files[0].name;
+                    }
+
+                    // if warnings
+                    if (data.hasWarnings) {
+                        console.log(data.warnings);
+
+                        item.html.removeClass('upload-successful').addClass('upload-failed');
+                        // go out from success function by calling onError function
+                        // in this case we have a animation there
+                        // you can also response in PHP with 404
+                        return this.onError ? this.onError(item) : null;
+                    }
+
+                    item.html.find('.column-actions').append('<a class="fileuploader-action fileuploader-action-remove fileuploader-action-success" title="Remove"><i></i></a>');
+                    setTimeout(function () {
+                        item.html.find('.progress-bar2').fadeOut(400);
+                    }, 400);
+                },
+                onError: function (item) {
+                    var progressBar = item.html.find('.progress-bar2');
+
+                    if (progressBar.length > 0) {
+                        progressBar.find('span').html("0%");
+                        progressBar.find('.fileuploader-progressbar .bar').width("0%");
+                        item.html.find('.progress-bar2').fadeOut(400);
+                    }
+
+                    if (item.upload.status !== 'cancelled' && item.html.find('.fileuploader-action-retry').length === 0) {
+                        item.html.find('.column-actions').prepend(
+                            '<a class="fileuploader-action fileuploader-action-retry" title="Retry"><i></i></a>'
+                        );
+                    }
+                },
+                onProgress: function (data, item) {
+                    var progressBar = item.html.find('.progress-bar2');
+
+                    if (progressBar.length > 0) {
+                        progressBar.show();
+                        progressBar.find('span').html(data.percentage + "%");
+                        progressBar.find('.fileuploader-progressbar .bar').width(data.percentage + "%");
+                    }
+                },
+                onComplete: null
+            },
+            onRemove: function (item) {
+                $(me.$el).find('.fileuploader-input').show();
+                $.post('http://www.gopala.fr/remove.php', {
+                    file: item.name
+                });
+            },
+            beforeSelect: function (files, listEl, parentEl, newInputEl, inputEl) {
+                console.log(newInputEl);
+                $(newInputEl).hide();
+                return true;
+            },
+            captions: {
+                removeConfirmation: 'Êtes-vous sûr de vouloir supprimer ce fichier ?'
             }
         });
     }
